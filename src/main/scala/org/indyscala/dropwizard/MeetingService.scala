6c0750c7888ad3aa4177ba0971f732cff2723403
Brad Fritz <brad@fewerhassles.com>
2012-12-03 11:23:16 -0500

    tweaks to archetype project for DW 0.6.x

--- src/main/scala/org/indyscala/dropwizard/MeetingService.scala
+++ src/main/scala/org/indyscala/dropwizard/MeetingService.scala
@@ -1,14 +1,19 @@
 package org.indyscala.dropwizard
 
 import com.yammer.dropwizard.ScalaService
-import com.yammer.dropwizard.config.Environment
+import com.yammer.dropwizard.bundles.ScalaBundle
+import com.yammer.dropwizard.config.{Bootstrap,Environment}
 
 object MeetingService 
-  extends ScalaService[MeetingConfiguration]("Meeting") {
+  extends ScalaService[MeetingConfiguration] {
 
-  def initialize(conf: MeetingConfiguration, env: Environment) {
-    // TODO: implement service
+  override def initialize(bootstrap: Bootstrap[MeetingConfiguration]) {
+    bootstrap.setName("meeting")
+    bootstrap.addBundle(new ScalaBundle)
   }
 
+  override def run(config: MeetingConfiguration, env: Environment) {
+    // TODO: implement service
+  }
 }
 
